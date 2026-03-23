55ab0f2731779cff30af571d1215633590d87a6c
Friasru <albertofriasru@gmail.com>
2026-03-23 05:24:10 -0700

    example: what the code does

---
+++ test.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	_printf("Hello World\n");
+	_printf("My name is %s\n", "Alberto");
+	_printf("I am %d years old\n", 19);
+	_printf("My favorite letter is %c\n", 'A');
+	
+	return (0);
+}
